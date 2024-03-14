587daf1d95969203e03f0f0cc4475d9cec06f8c6
Ge Junges <ge.junges@gmail.com>
2024-03-14 20:17:36 +0100

    Add all items to NavBar

--- components/navbar/NavBar.vue
+++ components/navbar/NavBar.vue
@@ -1,11 +1,12 @@
 <template>
   <nav class="grid grid-cols-12 gap-4">
     <div class="py-4 pr-4 col-start-2 col-span-2">
-      <div>Marieli</div>
+      <div class="text-center text-dark-brown font-alexBrush text-4xl">Marieli Ros</div>
+      <div class="text-center text-gold font-abhayaLibre text-xl">Psicóloga Clínica</div>
     </div>
-    <div class="col-start-5 col-end-12 flex flex-row">
-      <div v-for="(item, index) in menuItems" :key="index" class="p-4 hover:bg-red-500">
-        {{ item.label }}
+    <div class="py-4 pl-4 col-start-5 col-end-12 flex flex-row">
+      <div v-for="(item, index) in menuItems" :key="index" class="p-4 hover:bg-beige" :class="item.icon">
+        <span class="ml-2 text-dark-brown font-abhayaLibre font-bold">{{ item.label }}</span>
       </div>
     </div>
   </nav>
@@ -13,9 +14,11 @@
 
 <script setup lang="ts">
 const menuItems = [
-  { label: 'Home', id: '#home' },
-  { label: 'Quem sou', id: '#quem-sou' },
-  { label: 'Psicoterapia', id: '#psicoterapia' },
-  { label: 'Home', id: '#home' },
+  { label: 'Home', id: '#home', icon: 'pi pi-home' },
+  { label: 'Quem sou', id: '#quem-sou', icon: 'pi pi-heart' },
+  { label: 'Psicoterapia', id: '#psicoterapia', icon: 'pi pi-star' },
+  { label: 'Atendimento', id: '#atendimento', icon: 'pi pi-book' },
+  { label: 'Agendamento', id: '#agendamento', icon: 'pi pi-whatsapp' },
+  { label: 'Contato', id: '#contato', icon: 'pi pi-envelope' },
 ];
 </script>
