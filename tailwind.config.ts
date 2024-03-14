048134f410853bdf47c7ab4f87faf6e50a677e0c
Ge Junges <ge.junges@gmail.com>
2024-03-14 20:16:54 +0100

    Configure Icos, Colors and FontFamily

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,27 @@ export default <Partial<Config>>{
   content: [],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+        alexBrush: ['Alex Brush'],
+        cinzel: ['Cinzel'],
+        abhayaLibre: ['Abhaya Libre'],
+      },
       colors: {
         ...colors,
+        gray: '#393837',
+        beige: '#b7a292',
+        cream: '#e5e0d4',
+        'medium-brown': '#3d290f',
+        'dark-brown': '#271b0b',
+        'light-brown': '#775233',
+        'dark-blue': '#171d22',
+        blue: '#364f64',
         gold: '#b87c1c',
+        brickred: '#a95230',
+        'medium-green': '#0c3f3f',
+        turquoise: '#2b6061',
+        'light-turquoise': '#649e9e',
       },
     },
   },
