896f83ea858892a71e7b0d79cddb48b463b9c550
Ge Junges <ge.junges@gmail.com>
2024-03-16 14:02:33 +0100

    Add Hamburger Menu

---
+++ components/navbar/HamburgerMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="md:hidden col-start-6 col-span-2 pt-4">
+    <div class="flex justify-end pr-2">
+      <button type="button" class="text-dark-brown font-extrabold" @click="toggleNav">
+        <span class="pi pi-align-justify"></span>
+      </button>
+    </div>
+    <div class="absolute bg-light-brown bg-opacity-40 rounded-md pt-6 px-2 flex flex-col items-end">
+      <ul
+        v-for="(item, index) in menuItems"
+        :key="index"
+        class="relative nav-items hover:bg-beige"
+        :class="showMenu ? 'flex' : 'hidden'"
+      >
+        <li :class="item.icon" class="opacity-100 pb-3">
+          <span class="ml-1 text-dark-brown font-abhayaLibre font-bold text-lg pr-3">{{ item.label }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+const menuItems = [
+  { label: 'Home', id: '#home', icon: 'pi pi-home' },
+  { label: 'Quem sou', id: '#quem-sou', icon: 'pi pi-heart' },
+  { label: 'Psicoterapia', id: '#psicoterapia', icon: 'pi pi-star' },
+  { label: 'Atendimento', id: '#atendimento', icon: 'pi pi-book' },
+  { label: 'Agendamento', id: '#agendamento', icon: 'pi pi-whatsapp' },
+  { label: 'Contato', id: '#contato', icon: 'pi pi-envelope' },
+];
+
+const showMenu = ref(false);
+const toggleNav = () => (showMenu.value = !showMenu.value);
+</script>
--- components/navbar/NavBar.vue
+++ components/navbar/NavBar.vue
@@ -1,18 +1,20 @@
 <template>
-  <nav class="grid grid-cols-12 gap-4">
-    <div class="py-4 pr-4 col-start-2 col-span-2">
-      <div class="text-center text-dark-brown font-alexBrush text-4xl">Marieli Ros</div>
-      <div class="text-center text-gold font-abhayaLibre text-xl">Psicóloga Clínica</div>
+  <nav class="grid grid-cols-8 gap-1 sm:grid-cols-12 sm:gap-4">
+    <div class="py-4 pr-4 col-start-2 col-span-3">
+      <div class="text-center text-dark-brown font-alexBrush text-3xl sm:text-4xl">Marieli Ros</div>
+      <div class="text-center text-gold font-abhayaLibre text-sm sm:text-1xl">Psicóloga Clínica</div>
     </div>
-    <div class="py-4 pl-4 col-start-5 col-end-12 flex flex-row">
-      <div v-for="(item, index) in menuItems" :key="index" class="p-4 hover:bg-beige" :class="item.icon">
+    <div class="max-sm:hidden py-3 pl-3 col-start-5 col-end-12 flex flex-row justify-stretch">
+      <div v-for="(item, index) in menuItems" :key="index" class="p-3 hover:bg-beige" :class="item.icon">
         <span class="ml-2 text-dark-brown font-abhayaLibre font-bold">{{ item.label }}</span>
       </div>
     </div>
+    <HamburgerMenu />
   </nav>
 </template>
 
 <script setup lang="ts">
+import HamburgerMenu from '../components/navbar/HamburgerMenu.vue';
 const menuItems = [
   { label: 'Home', id: '#home', icon: 'pi pi-home' },
   { label: 'Quem sou', id: '#quem-sou', icon: 'pi pi-heart' },
