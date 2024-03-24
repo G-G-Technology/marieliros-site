43c2859f782637a1e0b4ccd75787a827f899e721
Ge Junges <ge.junges@gmail.com>
2024-03-24 12:26:44 +0100

    Add OfferedServices compoment and improve stiles

---
+++ components/services/OfferedServices.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="grid grid-cols-8 gap-1 sm:grid-cols-12 sm:gap-4">
+    <section>
+      <div class="col-start-2 col-span-6 md:col-start-2 md:col-span-10 flex flex-col items-start">
+        <div class="text-dark-brown font-cinzel text-xl md:text-3xl font-bold pb-2 capitalize">
+          alguns exemplos de como posso te ajudar
+        </div>
+        <h3 class="text-brickred font-abhayaLibre text-xl md:text-1xl font-bold pb-2 capitalize">Ansiedade</h3>
+        <div class="text-dark-brown font-abhayaLibre text-lg md:text-xl text-justify pb-2">
+          A Gestalt-terapia ajuda as pessoas a se concentrarem no presente. Isso permite que os indivíduos descubram
+          quais pensamentos, sentimentos ou comportamentos imediatos podem deixá-los ansiosos.
+        </div>
+
+        <h3 class="text-brickred font-abhayaLibre text-xl md:text-1xl font-bold pb-2 capitalize">
+          Problemas de autoestima
+        </h3>
+        <div class="text-dark-brown font-abhayaLibre text-lg md:text-xl text-justify pb-2">
+          A Gestalt-terapia ajuda as pessoas a se concentrarem no presente. Isso permite que os indivíduos descubram
+          quais pensamentos, sentimentos ou comportamentos imediatos podem deixá-los ansiosos.
+        </div>
+
+        <h3 class="text-brickred font-abhayaLibre text-xl md:text-1xl font-bold pb-2 capitalize">Depressão</h3>
+        <div class="text-dark-brown font-abhayaLibre text-lg md:text-xl text-justify pb-2">
+          Como a gestalt-terapia se concentra na autoconsciência, ela pode ajudar as pessoas com depressão a tomarem
+          consciência de possíveis situações estressantes que desencadeiam sua depressão.
+        </div>
+
+        <h3 class="text-brickred font-abhayaLibre text-xl md:text-1xl font-bold pb-2 capitalize">
+          Dificuldades de relacionamento
+        </h3>
+        <div class="text-dark-brown font-abhayaLibre text-lg md:text-xl text-justify pb-2">
+          A Gestalt-terapia pode ajudar os casais a reconhecer comportamentos destrutivos subjacentes que podem estar
+          impactando negativamente seus relacionamentos. Isso pode ajudá-los a trabalhar no sentido de fazer mudanças
+          positivas em seus relacionamentos e estilos de vida individuais.
+        </div>
+
+        <h3 class="text-brickred font-abhayaLibre text-xl md:text-1xl font-bold pb-2">
+          Problemas de saúde comportamental
+        </h3>
+        <div class="text-dark-brown font-abhayaLibre text-lg md:text-xl text-justify pb-0">
+          A Gestalt-terapia pode ajudar pessoas com problemas comportamentais, como transtorno bipolar. É também um
+          método terapêutico eficaz para tratar abuso de substâncias, jogo compulsivo e TEPT. Pessoas que lutam com
+          essas condições podem desenvolver uma nova visão da vida por meio da Gestalt-terapia e ver uma mudança
+          positiva em seu estilo de vida.
+        </div>
+      </div>
+      <div class="col-start-4 col-span-4 md:col-start-6 md:col-span-3 pt-0">
+        <ButtonComponent :button-lable="'Quero Agendar Uma Sessão'" />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ButtonComponent from '../components/common/ButtonComponent.vue';
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,11 @@
         <NavBar />
       </header>
       <main>
-        <AboutMe id="AboutMe" class="py-10" />
-        <AboutAppointment id="AboutAppointment" class="py-10" />
-        <GestaltTherapy id="GestaltTherapy" class="py-10" />
-        <ContactSection id="ContactSection" class="py-10" />
+        <AboutMe id="AboutMe" class="mt-16 md:mt-24" />
+        <AboutAppointment id="AboutAppointment" class="mt-16 md:mt-24" />
+        <GestaltTherapy id="GestaltTherapy" class="mt-16 md:mt-24" />
+        <OfferedServices id="OfferedServices" class="mt-16 md:mt-24" />
+        <ContactSection id="ContactSection" class="mt-16 md:mt-24" />
       </main>
     </div>
   </div>
@@ -19,5 +20,6 @@ import NavBar from '../components/navbar/NavBar.vue';
 import AboutMe from '../components/about/AboutMe.vue';
 import AboutAppointment from '../components/appointment/AboutAppointment.vue';
 import GestaltTherapy from '../components/therapy/GestaltTherapy.vue';
+import OfferedServices from '../components/services/OfferedServices.vue';
 import ContactSection from '../components/contacts/ContactSection.vue';
 </script>
