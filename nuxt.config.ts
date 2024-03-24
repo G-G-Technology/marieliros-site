0f0cfa742dc92f3bfbceab618cd4908f230255c7
Ge Junges <ge.junges@gmail.com>
2024-03-24 18:06:18 +0100

    nuxt loses CSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,6 @@ export default defineNuxtConfig({
     strict: true,
     typeCheck: true,
   },
-  css: ['./assets/css/tailwind.css', '@/assets/css/main.css', 'primeicons/primeicons.css'],
+  css: ['~/assets/css/tailwind.css', '@/assets/css/main.css', 'primeicons/primeicons.css'],
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
 });
