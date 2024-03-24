86733d2444e19e5c9595e105ddc1773ba2203286
Ge Junges <ge.junges@gmail.com>
2024-03-24 19:18:30 +0100

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    buildAssetsDir: '/marieliros/_nuxt/',
+  },
   devtools: { enabled: true },
   typescript: {
     strict: true,
     typeCheck: true,
   },
-  css: ['~/assets/css/tailwind.css', '@/assets/css/main.css', 'primeicons/primeicons.css'],
+  css: ['~/assets/css/tailwind.css', '@/assets/css/main.css', '~/node_modules/primeicons/primeicons.css'],
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
 });
