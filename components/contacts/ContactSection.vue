0b8a390b310e61e4bdb4068d349a470392b6517e
Ge Junges <ge.junges@gmail.com>
2024-03-25 17:23:35 +0100

    small things

--- components/contacts/ContactSection.vue
+++ components/contacts/ContactSection.vue
@@ -4,7 +4,7 @@
       class="grid grid-cols-8 gap-1 md:grid-cols-12 md:gap-4 bg-[url('./img/ContactPage.png')] w-full h-full bg-cover bg-center bg-no-repeat"
     >
       <div class="col-span-8 md:col-span-12 bg-white bg-opacity-80 flex flex-col items-center">
-        <h1 class="text-dark-brown font-cinzel font-bold text-xs md:text-lg pt-6 md:pt-16 text-center">
+        <h1 class="text-dark-brown font-cinzel font-bold text-xs md:text-lg pt-12 md:pt-32 text-center">
           Dúvidas, Perguntas, Agendar uma Sessão
         </h1>
         <h2 class="text-dark-brown font-cinzel font-bold text-xs md:text-lg pt-4 md:pt-16 text-center">
@@ -12,7 +12,7 @@
           <p>Retornaremos a você em menos de 24 horas.</p>
         </h2>
 
-        <div class="flex flex-row pt-3 md:pt-8 pb-6">
+        <div class="flex flex-row pt-3 md:pt-8 pb-12 md:pb-20">
           <NuxtLink
             to="mailto:psi.marieliros@gmail.com?subject=Agendamento%20de%20consulta&body=Ol%C3%A1%20Marieli%2C%20%0AVenho%20atrav%C3%A9s%20to%20seu%20website%20e%20gostaria%20de%20agendar%20uma%20consulta.%0A"
             target="_blank"
